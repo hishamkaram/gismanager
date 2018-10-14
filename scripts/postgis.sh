7b1e2d5a46e148dd4522df85d613b410827a2d8f
hisham waleed karam <hisham.karam@cartologic.com>
2018-10-14 04:02:31 +0200

    fix postgis port

--- scripts/postgis.sh
+++ scripts/postgis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PGPASSWORD="golang"
-$PWD/scripts/wait.sh -h localhost -p 5436 -t 600 -- echo "Postgis Ready!!!!"
+$PWD/scripts/wait.sh -h localhost -p 5432 -t 600 -- echo "Postgis Ready!!!!"
